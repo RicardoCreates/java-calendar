4b095a40f0376f977f94520c67dd78793ac6f5ff
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-08-17 11:47:27 +0200

    final

--- src/main/java/de/ricardo/Main.java
+++ src/main/java/de/ricardo/Main.java
@@ -1,18 +1,35 @@
-package de.ricardo;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("January");
-        System.out.println("February");
-        System.out.println("March");
-        System.out.println("April");
-        System.out.println("May");
-        System.out.println("June");
-        System.out.println("July");
-        System.out.println("August");
-        System.out.println("September");
-        System.out.println("October");
-        System.out.println("November");
-        System.out.println("December");
+        printCalendarForMonth("January", 31, 0);
+        printCalendarForMonth("February", 28, 3);
+        printCalendarForMonth("March", 31, 4);
+        printCalendarForMonth("April", 30, 0);
+        printCalendarForMonth("May", 31, 2);
+        printCalendarForMonth("June", 30, 5);
+        printCalendarForMonth("July", 31, 0);
+        printCalendarForMonth("August", 31, 3);
+        printCalendarForMonth("September", 30, 6);
+        printCalendarForMonth("October", 31, 1);
+        printCalendarForMonth("November", 30, 4);
+        printCalendarForMonth("December", 31, 6);
+    }
+
+    public static void printCalendarForMonth(String monthName, int daysInMonth, int offsetDays) {
+        System.out.println(monthName);
+        System.out.println("Mo Tu We Th Fr Sa Su");
+
+        for (int i = 0; i < offsetDays; i++) {
+            System.out.print("   ");
+        }
+
+        int day = 1;
+        while (day <= daysInMonth) {
+            for (int i = offsetDays; i < 7 && day <= daysInMonth; i++) {
+                System.out.printf("%2d ", day++);
+            }
+            offsetDays = 0;
+            System.out.println();
+        }
+        System.out.println();
     }
 }
